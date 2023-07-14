2d6339740d756cd3fc1055be5f19882ba2c18f4c
Eduardo Malpeli <tchatcho66@hotmail.com>
2023-07-13 22:57:39 -0300

    Changing PORT

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -7,6 +7,6 @@ COPY . .
 # install npm dependencies and pm2
 RUN npm install --only=production && npm install -g pm2
 # expose port 80 for our server to run on
-EXPOSE 80
+EXPOSE 8080
 # command to start our server
 CMD [ "pm2-runtime", "start", "server.js" ]
