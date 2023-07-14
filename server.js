13333225e8641d0bb4588cd0aa16a9eeff71ba7a
Eduardo Malpeli <tchatcho66@hotmail.com>
2023-07-14 16:50:30 -0300

    TCP client for testing cluster

--- server.js
+++ server.js
@@ -1,56 +1,37 @@
-const http = require('http');
-const url = require('url');
-global.PORT = 8080;
+const net = require('net');
+const port = 7070;
+const port2 = 5001;
+const host = '127.0.0.1';
 
-function getData(req) {
-  return new Promise((resolve, reject) => {
-    const queryObject = url.parse(req.url, true).query;
-    let body = '';
-    function res() {
-      resolve(Object.assign({}, JSON.parse(body||"{}"), queryObject));
-    };
-    if (req.method == 'POST') {
+const server = net.createServer();
+server.listen(port2, host, () => {
+  console.log('TCP Server is running on port ' + port2 + '.');
+});
+server.listen(port, host, () => {
+  console.log('TCP Server is running on port ' + port + '.');
+});
 
-      req.on('data', function (data) {
-        body += data;
-        // Too much POST data, kill the connection!
-        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-        if (body.length > 1e6) req.connection.destroy();
-      });
+let sockets = [];
 
-      req.on('end', () => res());
-    } else {
-      res();
-    };
+server.on('connection', function(sock) {
+  console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+  sockets.push(sock);
+
+  sock.on('data', function(data) {
+    console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to all the connected, the client will receive it as data from the server
+    sockets.forEach(function(sock, index, array) {
+      sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+    });
   });
-};
 
+    // Add a 'close' event handler to this instance of socket
+  sock.on('close', function(data) {
+    let index = sockets.findIndex(function(o) {
+      return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+    })
+    if (index !== -1) sockets.splice(index, 1);
+    console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+  });
+});
 
-http.globalAgent.maxSockets = Infinity;
-
-http
-.createServer(async (req, res) => {
-  const headers = {
-    'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
-    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-    'Access-Control-Max-Age': 2592000, // 30 days
-    /** add other headers as per requirement */
-  };
-  let data = await getData(req);
-  data.url = req.url;
-  let response = JSON.stringify(data);
-  data.format = 'json';
-  const pre = data.format === 'json' ? 'application/' : 'text/';
-  headers['Content-Type'] = pre+data.format.toLowerCase()
-  res.writeHead(200, headers);
-  try {
-    res.end(response);
-  } catch (e) {
-    res.end('Error');
-  };
-
-})
-.listen(global.PORT);
-
-console.log(`app listening on port ${global.PORT}`)
-if (global.DEV) console.log('Running in DEV mode')
