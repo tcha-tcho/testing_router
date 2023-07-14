89343f9731b3964393efa6bcdaf9fb1c25188404
Eduardo Malpeli <tchatcho66@hotmail.com>
2023-07-14 18:15:50 -0300

    Expose all ports

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY . .
 # install npm dependencies and pm2
 RUN npm install --only=production && npm install -g pm2
 # expose port 80 for our server to run on
-EXPOSE 80 8080 7070 5001
+# EXPOSE 80 8080 7070 5001 Expose all ports...
 # command to start our server
 CMD [ "pm2-runtime", "start", "server.js" ]
--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 const net = require('net');
 const client = new net.Socket();
-const port = 5001;
-const host = '127.0.0.1';
+const port = 8080;
+// const host = '127.0.0.1';
+// const host = '34.27.121.244'; // balancer
+const host = '34.132.145.147'; // instance
 
 client.connect(port, host, function() {
   console.log('Connected');
