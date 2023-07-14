716ae02dc88fc25021acd395605cf7f0cd8e18cd
Eduardo Malpeli <tchatcho66@hotmail.com>
2023-07-14 18:55:26 -0300

    Changing host to match firewall

--- client.js
+++ client.js
@@ -2,8 +2,8 @@ const net = require('net');
 const client = new net.Socket();
 const port = 5001;
 // const host = '127.0.0.1';
-// const host = '34.27.121.244'; // balancer
-const host = '34.132.145.147'; // instance
+const host = '34.27.121.244'; // balancer
+// const host = '34.132.145.147'; // instance
 
 client.connect(port, host, function() {
   console.log('Connected');
