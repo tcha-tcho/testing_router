4c1d7bc34a6d57235f7784047c476de2af93057f
Eduardo Malpeli <tchatcho66@hotmail.com>
2023-07-13 22:55:39 -0300

    Dockerfile

---
+++ Dockerfile.sh
@@ -0,0 +1,12 @@
+# version of node to use
+FROM node:18
+# define working directory for docker
+WORKDIR /usr/src/app
+# copy all our source code into the working directory
+COPY . .
+# install npm dependencies and pm2
+RUN npm install --only=production && npm install -g pm2
+# expose port 80 for our server to run on
+EXPOSE 80
+# command to start our server
+CMD [ "pm2-runtime", "start", "server.js" ]
